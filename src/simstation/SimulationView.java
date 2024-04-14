077fe675dc8a4f9ec95645a431bfd4b006402288
Jason <jdoria702@gmail.com>
2024-04-14 11:51:08 -0700

    minimal change - change update to abstract class

--- src/simstation/SimulationView.java
+++ src/simstation/SimulationView.java
@@ -6,4 +6,11 @@ public class SimulationView extends View {
     public SimulationView(Model newModel) {
         super(newModel);
     }
+
+    /*
+    public static int FRAME_WIDTH = 830;
+    public static int FRAME_HEIGHT = 500;
+
+    I think the view should be half these values ^
+     */
 }
