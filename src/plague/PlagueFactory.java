8ba6b57ad68281dfb2840de8ad87481dcf09e141
Liza Podkamenna <eliza.podk@gmail.com>
2024-04-17 09:44:39 -0700

    fixed up plague factory and plague view

--- src/plague/PlagueFactory.java
+++ src/plague/PlagueFactory.java
@@ -1,22 +1,16 @@
-//package plague;
-//
-//import mvc.Model;
-//import mvc.View;
-//import simstation.SimStationFactory;
-//
-//public class PlagueFactory extends SimStationFactory {
-//    @Override
-//    public View getView(Model model) {
-//        return new PlagueView((PlagueSimulation) model);
-//    }
-//
-//    @Override
-//    public Model makeModel() {
-//        return new PlagueSimulation();
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Plague Simulation";
-//    }
-//}
+package plague;
+
+import mvc.Model;
+import simstation.SimStationFactory;
+
+public class PlagueFactory extends SimStationFactory {
+    @Override
+    public Model makeModel() {
+        return new PlagueSimulation();
+    }
+
+    @Override
+    public String getTitle() {
+        return "Plague";
+    }
+}
--- src/plague/PlagueView.java
+++ src/plague/PlagueView.java
@@ -1,18 +1,14 @@
-//package plague;
-//
-//import simstation.Agent;
-//import simstation.Heading;
-//import simstation.Simulation;
-//
-//public class PlagueView extends Agent {
-//    public void PlagueAgent(String name, PlagueSimulation world, boolean infected) {
-//        this(name, world);
-//        this.infected = infected;
-//    }
-//
-//    private boolean infected;
-//
-//    public boolean isInfected() {
-//        return infected;
-//    }
-//}
+package plague;
+
+import simstation.Agent;
+import simstation.Heading;
+import simstation.Simulation;
+import mvc.AppPanel;
+import simstation.SimulationPanel;
+
+public class PlagueView  {
+    public static void main(String[] args) {
+        AppPanel panel = new SimulationPanel(new PlagueFactory());
+        panel.display();
+    }
+}
