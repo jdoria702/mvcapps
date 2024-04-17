12e494d84d99c073321007d5dcb7333b4f841a06
Liza Podkamenna <eliza.podk@gmail.com>
2024-04-16 23:08:28 -0700

    general template for the plague package

--- src/plague/PlagueView.java
+++ src/plague/PlagueView.java
@@ -1,4 +1,18 @@
-package plague;
-
-public class PlagueView {
-}
+//package plague;
+//
+//import simstation.Agent;
+//import simstation.Heading;
+//import simstation.Simulation;
+//
+//public class PlagueView extends Agent {
+//    public void PlagueAgent(String name, PlagueSimulation world, boolean infected) {
+//        this(name, world);
+//        this.infected = infected;
+//    }
+//
+//    private boolean infected;
+//
+//    public boolean isInfected() {
+//        return infected;
+//    }
+//}
